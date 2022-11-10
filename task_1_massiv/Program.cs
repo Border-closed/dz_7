e151c1250e0f40fb129fd76b639064db0f81ed88
Dmitrii <directsdi@gmail.com>
2022-11-10 20:56:24 +0500

    вывод рандомного массива

---
+++ task_1_massiv/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int vvod(string text)
+{
+    Console.WriteLine(text);
+
+        int V = int.Parse (Console.ReadLine());
+        return V;
+}
+try
+{
+int m = vvod("введите количество строк в массиве (>0):  ");
+int n = vvod("введите количество столбцов в массиве(>0):  ");
+if (m>0 & n>0)
+{
+double[,]massiv = new double[m,n];
+Random rnd = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        massiv [i,j] = Convert.ToDouble (rnd.Next(-1000,1000)/10.0);
+        Console.Write($"{massiv[i,j]}   ");
+    }
+    Console.WriteLine();
+}
+}
+else
+{
+    Console.WriteLine("Вы ввели не число или число <=0");
+}
+}
+catch
+{
+    Console.WriteLine("Вы ввели не число или число <=0");
+}
